543d3ff4683948db5744cfe56d3e1c06d721d831
Pavel Stepanov <paulkreuzmann@gmail.com>
2021-12-29 18:25:55 +0600

    init

---
+++ default.nix
@@ -0,0 +1,33 @@
+{ pkgs ? <nixpkgs> {} }:
+
+with pkgs;
+
+{
+  fetchdarcs = { url, rev ? null, context ? null, patch ? null, sha256 ? "" }:
+    stdenvNoCC.mkDerivation {
+      name = "fetchdarcs";
+      builder = ./builder.sh;
+      nativeBuildInputs = [ cacert darcs ];
+
+      outputHashAlgo = "sha256";
+      outputHashMode = "recursive";
+      outputHash = sha256;
+
+      inherit url rev context patch;
+    };
+  nix-prefetch-darcs = stdenv.mkDerivation {
+    name = "nix-prefetch-darcs";
+    version = "0.0.1";
+    nativeBuildInputs = [ makeWrapper ];
+    buildInputs = [ darcs gnused nix ];
+    src = ./.;
+    dontUnpack = true;
+    preferLocalBuild = true;
+    installPhase = ''
+      install -vD ${src} $out/bin/$name
+      wrapProgram $out/bin/$name \
+        --prefix PATH : ${lib.makeBinPath buildInputs} \
+        --set HOME /homeless-shelter
+    '';
+  };
+}
